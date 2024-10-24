57c50fce77c034a373b20f8ff2ba115db5319bae
rahat <rahat14048@gmail.com>
2024-10-25 00:38:46 +0600

    upgrade front end

--- ProductCartApp/src/main/java/com/productCart/controller/AdminController.java
+++ ProductCartApp/src/main/java/com/productCart/controller/AdminController.java
@@ -6,11 +6,13 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.domain.Page;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.ObjectUtils;
@@ -24,25 +26,69 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.productCart.model.Category;
 import com.productCart.model.Product;
+import com.productCart.model.ProductOrder;
+import com.productCart.model.UserDtls;
+import com.productCart.service.CartService;
 import com.productCart.service.CategoryService;
+import com.productCart.service.OrderService;
 import com.productCart.service.ProductService;
+import com.productCart.service.UserService;
+import com.productCart.util.CommonUtil;
+import com.productCart.util.OrderStatus;
 
 import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+
 	@Autowired
 	private CategoryService categoryService;
+
 	@Autowired
 	private ProductService productService;
-	
-	
+
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private CartService cartService;
+
+	@Autowired
+	private OrderService orderService;
+
+	@Autowired
+	private CommonUtil commonUtil;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@ModelAttribute
+	public void getUserDetails(Principal p, Model m) {
+		if (p != null) {
+			String email = p.getName();
+			UserDtls userDtls = userService.getUserByEmail(email);
+			m.addAttribute("user", userDtls);
+			Integer countCart = cartService.getCountCart(userDtls.getId());
+			m.addAttribute("countCart", countCart);
+		}
+
+		List<Category> allActiveCategory = categoryService.getAllActiveCategory();
+		m.addAttribute("categorys", allActiveCategory);
+	}
+
 	@GetMapping("/")
 	public String index() {
 		return "admin/index";
 	}
-	
+
+	@GetMapping("/loadAddProduct")
+	public String loadAddProduct(Model m) {
+		List<Category> categories = categoryService.getAllCategory();
+		m.addAttribute("categories", categories);
+		return "admin/add_product";
+	}
+
 	@GetMapping("/category")
 	public String category(Model m, @RequestParam(name = "pageNo", defaultValue = "0") Integer pageNo,
 			@RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize) {
@@ -61,11 +107,10 @@ public class AdminController {
 		return "admin/category";
 	}
 
-	
 	@PostMapping("/saveCategory")
 	public String saveCategory(@ModelAttribute Category category, @RequestParam("file") MultipartFile file,
 			HttpSession session) throws IOException {
-		//get original file name and set on category table
+
 		String imageName = file != null ? file.getOriginalFilename() : "default.jpg";
 		category.setImageName(imageName);
 
@@ -80,15 +125,13 @@ public class AdminController {
 			if (ObjectUtils.isEmpty(saveCategory)) {
 				session.setAttribute("errorMsg", "Not saved ! internal server error");
 			} else {
-				//locate the folder inside the classpath and gets the file object
+
 				File saveFile = new ClassPathResource("static/img").getFile();
-				
-				//Path Creation
+
 				Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + "category_img" + File.separator
 						+ file.getOriginalFilename());
 
 				// System.out.println(path);
-				//copies the input stream of the uploaded file to the constructed path 
 				Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
 
 				session.setAttribute("succMsg", "Saved successfully");
@@ -97,7 +140,7 @@ public class AdminController {
 
 		return "redirect:/admin/category";
 	}
-	
+
 	@GetMapping("/deleteCategory/{id}")
 	public String deleteCategory(@PathVariable int id, HttpSession session) {
 		Boolean deleteCategory = categoryService.deleteCategory(id);
@@ -110,7 +153,7 @@ public class AdminController {
 
 		return "redirect:/admin/category";
 	}
-	
+
 	@GetMapping("/loadEditCategory/{id}")
 	public String loadEditCategory(@PathVariable int id, Model m) {
 		m.addAttribute("category", categoryService.getCategoryById(id));
@@ -152,14 +195,7 @@ public class AdminController {
 
 		return "redirect:/admin/loadEditCategory/" + category.getId();
 	}
-	
-	@GetMapping("/loadAddProduct")
-	public String loadAddProduct(Model m) {
-		List<Category> categories = categoryService.getAllCategory();
-		m.addAttribute("categories", categories);
-		return "admin/add_product";
-	}
-	
+
 	@PostMapping("/saveProduct")
 	public String saveProduct(@ModelAttribute Product product, @RequestParam("file") MultipartFile image,
 			HttpSession session) throws IOException {
@@ -188,38 +224,38 @@ public class AdminController {
 
 		return "redirect:/admin/loadAddProduct";
 	}
-	
+
 	@GetMapping("/products")
 	public String loadViewProduct(Model m, @RequestParam(defaultValue = "") String ch,
 			@RequestParam(name = "pageNo", defaultValue = "0") Integer pageNo,
 			@RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize) {
 
-		List<Product> products = null;
-		if (ch != null && ch.length() > 0) {
-			products = productService.searchProduct(ch);
-		} else {
-			products = productService.getAllProducts();
-		}
-		m.addAttribute("products", products);
-
-//		Page<Product> page = null;
+//		List<Product> products = null;
 //		if (ch != null && ch.length() > 0) {
-//			page = productService.searchProductPagination(pageNo, pageSize, ch);
+//			products = productService.searchProduct(ch);
 //		} else {
-//			page = productService.getAllProductsPagination(pageNo, pageSize);
+//			products = productService.getAllProducts();
 //		}
-//		m.addAttribute("products", page.getContent());
-//
-//		m.addAttribute("pageNo", page.getNumber());
-//		m.addAttribute("pageSize", pageSize);
-//		m.addAttribute("totalElements", page.getTotalElements());
-//		m.addAttribute("totalPages", page.getTotalPages());
-//		m.addAttribute("isFirst", page.isFirst());
-//		m.addAttribute("isLast", page.isLast());
+//		m.addAttribute("products", products);
+
+		Page<Product> page = null;
+		if (ch != null && ch.length() > 0) {
+			page = productService.searchProductPagination(pageNo, pageSize, ch);
+		} else {
+			page = productService.getAllProductsPagination(pageNo, pageSize);
+		}
+		m.addAttribute("products", page.getContent());
+
+		m.addAttribute("pageNo", page.getNumber());
+		m.addAttribute("pageSize", pageSize);
+		m.addAttribute("totalElements", page.getTotalElements());
+		m.addAttribute("totalPages", page.getTotalPages());
+		m.addAttribute("isFirst", page.isFirst());
+		m.addAttribute("isLast", page.isLast());
 
 		return "admin/products";
 	}
-	
+
 	@GetMapping("/deleteProduct/{id}")
 	public String deleteProduct(@PathVariable int id, HttpSession session) {
 		Boolean deleteProduct = productService.deleteProduct(id);
@@ -255,6 +291,185 @@ public class AdminController {
 		return "redirect:/admin/editProduct/" + product.getId();
 	}
 
-
-	
+	@GetMapping("/users")
+	public String getAllUsers(Model m, @RequestParam Integer type) {
+		List<UserDtls> users = null;
+		if (type == 1) {
+			users = userService.getUsers("ROLE_USER");
+		} else {
+			users = userService.getUsers("ROLE_ADMIN");
+		}
+		m.addAttribute("userType",type);
+		m.addAttribute("users", users);
+		return "/admin/users";
 	}
+
+	@GetMapping("/updateSts")
+	public String updateUserAccountStatus(@RequestParam Boolean status, @RequestParam Integer id,@RequestParam Integer type, HttpSession session) {
+		Boolean f = userService.updateAccountStatus(id, status);
+		if (f) {
+			session.setAttribute("succMsg", "Account Status Updated");
+		} else {
+			session.setAttribute("errorMsg", "Something wrong on server");
+		}
+		return "redirect:/admin/users?type="+type;
+	}
+
+	@GetMapping("/orders")
+	public String getAllOrders(Model m, @RequestParam(name = "pageNo", defaultValue = "0") Integer pageNo,
+			@RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize) {
+//		List<ProductOrder> allOrders = orderService.getAllOrders();
+//		m.addAttribute("orders", allOrders);
+//		m.addAttribute("srch", false);
+
+		Page<ProductOrder> page = orderService.getAllOrdersPagination(pageNo, pageSize);
+		m.addAttribute("orders", page.getContent());
+		m.addAttribute("srch", false);
+
+		m.addAttribute("pageNo", page.getNumber());
+		m.addAttribute("pageSize", pageSize);
+		m.addAttribute("totalElements", page.getTotalElements());
+		m.addAttribute("totalPages", page.getTotalPages());
+		m.addAttribute("isFirst", page.isFirst());
+		m.addAttribute("isLast", page.isLast());
+
+		return "/admin/orders";
+	}
+
+	@PostMapping("/update-order-status")
+	public String updateOrderStatus(@RequestParam Integer id, @RequestParam Integer st, HttpSession session) {
+
+		OrderStatus[] values = OrderStatus.values();
+		String status = null;
+
+		for (OrderStatus orderSt : values) {
+			if (orderSt.getId().equals(st)) {
+				status = orderSt.getName();
+			}
+		}
+
+		ProductOrder updateOrder = orderService.updateOrderStatus(id, status);
+
+		try {
+			commonUtil.sendMailForProductOrder(updateOrder, status);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		if (!ObjectUtils.isEmpty(updateOrder)) {
+			session.setAttribute("succMsg", "Status Updated");
+		} else {
+			session.setAttribute("errorMsg", "status not updated");
+		}
+		return "redirect:/admin/orders";
+	}
+
+	@GetMapping("/search-order")
+	public String searchProduct(@RequestParam String orderId, Model m, HttpSession session,
+			@RequestParam(name = "pageNo", defaultValue = "0") Integer pageNo,
+			@RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize) {
+
+		if (orderId != null && orderId.length() > 0) {
+
+			ProductOrder order = orderService.getOrdersByOrderId(orderId.trim());
+
+			if (ObjectUtils.isEmpty(order)) {
+				session.setAttribute("errorMsg", "Incorrect orderId");
+				m.addAttribute("orderDtls", null);
+			} else {
+				m.addAttribute("orderDtls", order);
+			}
+
+			m.addAttribute("srch", true);
+		} else {
+//			List<ProductOrder> allOrders = orderService.getAllOrders();
+//			m.addAttribute("orders", allOrders);
+//			m.addAttribute("srch", false);
+
+			Page<ProductOrder> page = orderService.getAllOrdersPagination(pageNo, pageSize);
+			m.addAttribute("orders", page);
+			m.addAttribute("srch", false);
+
+			m.addAttribute("pageNo", page.getNumber());
+			m.addAttribute("pageSize", pageSize);
+			m.addAttribute("totalElements", page.getTotalElements());
+			m.addAttribute("totalPages", page.getTotalPages());
+			m.addAttribute("isFirst", page.isFirst());
+			m.addAttribute("isLast", page.isLast());
+
+		}
+		return "/admin/orders";
+
+	}
+
+	@GetMapping("/add-admin")
+	public String loadAdminAdd() {
+		return "/admin/add_admin";
+	}
+
+	@PostMapping("/save-admin")
+	public String saveAdmin(@ModelAttribute UserDtls user, @RequestParam("img") MultipartFile file, HttpSession session)
+			throws IOException {
+
+		String imageName = file.isEmpty() ? "default.jpg" : file.getOriginalFilename();
+		user.setProfileImage(imageName);
+		UserDtls saveUser = userService.saveAdmin(user);
+
+		if (!ObjectUtils.isEmpty(saveUser)) {
+			if (!file.isEmpty()) {
+				File saveFile = new ClassPathResource("static/img").getFile();
+
+				Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + "profile_img" + File.separator
+						+ file.getOriginalFilename());
+
+//				System.out.println(path);
+				Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+			}
+			session.setAttribute("succMsg", "Register successfully");
+		} else {
+			session.setAttribute("errorMsg", "something wrong on server");
+		}
+
+		return "redirect:/admin/add-admin";
+	}
+
+	@GetMapping("/profile")
+	public String profile() {
+		return "/admin/profile";
+	}
+
+	@PostMapping("/update-profile")
+	public String updateProfile(@ModelAttribute UserDtls user, @RequestParam MultipartFile img, HttpSession session) {
+		UserDtls updateUserProfile = userService.updateUserProfile(user, img);
+		if (ObjectUtils.isEmpty(updateUserProfile)) {
+			session.setAttribute("errorMsg", "Profile not updated");
+		} else {
+			session.setAttribute("succMsg", "Profile Updated");
+		}
+		return "redirect:/admin/profile";
+	}
+
+	@PostMapping("/change-password")
+	public String changePassword(@RequestParam String newPassword, @RequestParam String currentPassword, Principal p,
+			HttpSession session) {
+		UserDtls loggedInUserDetails = commonUtil.getLoggedInUserDetails(p);
+
+		boolean matches = passwordEncoder.matches(currentPassword, loggedInUserDetails.getPassword());
+
+		if (matches) {
+			String encodePassword = passwordEncoder.encode(newPassword);
+			loggedInUserDetails.setPassword(encodePassword);
+			UserDtls updateUser = userService.updateUser(loggedInUserDetails);
+			if (ObjectUtils.isEmpty(updateUser)) {
+				session.setAttribute("errorMsg", "Password not updated !! Error in server");
+			} else {
+				session.setAttribute("succMsg", "Password Updated sucessfully");
+			}
+		} else {
+			session.setAttribute("errorMsg", "Current Password incorrect");
+		}
+
+		return "redirect:/admin/profile";
+	}
+
+}
--- ProductCartApp/src/main/java/com/productCart/service/impl/CartServiceImpl.java
+++ ProductCartApp/src/main/java/com/productCart/service/impl/CartServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
 import com.productCart.model.Cart;
@@ -14,6 +15,7 @@ import com.productCart.repository.ProductRepository;
 import com.productCart.repository.UserRepository;
 import com.productCart.service.CartService;
 
+@Service
 public class CartServiceImpl implements CartService{
 	
 	@Autowired
